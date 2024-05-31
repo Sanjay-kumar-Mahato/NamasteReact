8f64ac5c17522537cdb81bb9fb84f6324b115694
Sanjay Kumar Mahato <sanjaykumarmahato4502@gmail.com>
2024-06-01 01:42:08 +0530

    episode1

---
+++ index2.js
@@ -0,0 +1,52 @@
+
+// async function getData(){
+//     return "Hi How Are You?";
+// }
+
+// const data = getData();
+// try{
+    
+// console.log(data);
+// // console.log(data.json());
+// console.log(typeof data);
+// }catch(error){
+//     console.log(Error(error)); 
+// }
+
+// const head = document.createElement("h1");
+// head.innerHTML = "Hello World";
+// const hello = document.getElementById("heading");
+// hello.appendChild(head);
+
+const text = React.createElement("h1",{},"Hello World with React");
+const reactHeading = ReactDOM.createRoot(document.getElementById("heading"));
+reactHeading.render(text);
+
+console.log(text);
+console.log(reactHeading);
+
+/*
+div #parent 
+    div #child
+        h3 "hi" 
+        h3 "hello" 
+    /div
+    div #child
+        h3 "vicky" 
+        h3 "speaking" 
+    /div
+/div
+*/
+const heading2 = React.createElement("div",{id:"Parent", className :"DOM"},
+    [React.createElement("div",{id:"Child", className : "DOM"},
+        [React.createElement("h3",{id:"Head2", className : "DOM"},"hi"),
+        React.createElement("h3",{id:"Head2", className : "DOM"},"hello")]
+    ),
+    React.createElement("div",{id:"Child", className : "DOM"},
+        [React.createElement("h3",{id:"Head2", className : "DOM"},"Vicky"),
+        React.createElement("h3",{id:"Head2", className : "DOM"},"speaking")]
+    )]
+);
+
+const dom2 = ReactDOM.createRoot(document.getElementById("heading2"));
+dom2.render(heading2);
